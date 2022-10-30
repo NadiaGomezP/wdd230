7e72607b63b3ab30d7dbaf6291fffde835bfceb7
Nadia Peralta <nadiaperaltadiaz@gmail.com>
2022-10-30 01:22:41 -0300

    week 7

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -65,4 +65,50 @@ let daynames = [
       document.getElementById("announcement").style.display="none";
     }
 
+    const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return;
+  }
+
+  img.src = src;
+  img.removeAttribute("data-src");
+}
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 200px 0px",
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+}, imgOptions);
+
+images.forEach((image) => {
+  imgObserver.observe(image);
+});
+
+
+const countDisplay = document.querySelector(".visits");
+const lastVisit = Number(window.localStorage.getItem("last-visit"));
+
+    const day= Date.now();
+
+    let difference = day - lastVisit;
+
+    let roundedDays = (difference/1000/60/60/24).toFixed(0);
+
+    localStorage.setItem("last-visit", day);
+
     
+	countDisplay.textContent = roundedDays
+
